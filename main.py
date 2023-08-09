465c0d3fd6d82a1f63274c679e6254d58b3ce681
BlackRose90 <fatoo869@gmail.com>
2023-08-10 00:44:32 +0300

    Calculate the areaCircle , Rectangle and areaSquare in object oriented programming in python

---
+++ main.py
@@ -0,0 +1,28 @@
+def areaOfrectangle(r):
+    return r*r
+width = 5  
+height = 10  
+area = width * height  
+print("Area of rectangle="+str(area)) 
+
+
+
+def areaOfSquare(s):
+    return s*s
+
+print("Enter the Side Length of Square: ", end="")
+l = float(input())
+a = areaOfSquare(l)
+print("\nArea = {:.2f}".format(a))
+
+
+
+def area_of_the_circle (Radius):   
+    area = Radius  
+    return area  
+  
+Radius = float (input ("Please enter the radius of the given circle: "))  
+print (" The area of the given circle is: ", area_of_the_circle (Radius))  
+
+
+
